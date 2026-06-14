984e2e6189bddfb983c3782e0a3bee031947416c
tfForster <Benno-Klan@gmx.de>
2026-06-14 23:21:37 +0200

    fix: merge migration branches

---
+++ migrations/versions/269ef89c8b45_merge_short_description_branch.py
@@ -0,0 +1,24 @@
+"""merge short_description branch
+
+Revision ID: 269ef89c8b45
+Revises: 17ad55fb99cc
+Create Date: 2026-06-14 22:12:37.302412
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '269ef89c8b45'
+down_revision = '17ad55fb99cc'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
