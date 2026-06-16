f2430ed3285f5e8a78d09f001062001fb5661fbe
tfForster <Benno-Klan@gmx.de>
2026-06-16 20:20:59 +0200

    fix: set correct down_revision for project model migration

--- migrations/versions/23c5c6704174_add_project_model.py
+++ migrations/versions/23c5c6704174_add_project_model.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '23c5c6704174'
-down_revision = None
+down_revision = 'c033a3336a08'
 branch_labels = None
 depends_on = None
 
